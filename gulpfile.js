ad217d191895959b43be4b22db8e678b4ab96c0c
Седона <enotov.92@list.ru>
2020-05-03 18:46:36 +0300

    исправил ошибки

--- gulpfile.js
+++ gulpfile.js
@@ -46,68 +46,67 @@ gulp.task("server", function () {
 });
 
 gulp.task("refresh", function (done){
-    server.reload();
-    done();
+  server.reload();
+  done();
 });
 
 gulp.task("start", gulp.series("css", "server"));
 
 gulp.task("images", function(){
-    return gulp.src("source/img/**/*.{png,jpg,svg}")
-        .pipe(imagemin([
-          imagemin.optipng({optimizationLevel: 1}),
-          imagemin.mozjpeg({progressive: true}),
-          imagemin.svgo()
-        ]))
-        .pipe(gulp.dest("source/img"))
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 1}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"))
 });
 
 gulp.task("webp", function () {
-    return gulp.src("source/img/**/**.{png,jpg}")
-        .pipe(webp({quality: 90}))
-        .pipe(gulp.dest("source/img"));
+  return gulp.src("source/img/**/**.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("sprite", function(){
-    return gulp.src("source/img/icon-*.svg")
-        .pipe(svgstore({
-          inlineSvg: true
-        }))
-        .pipe(rename("sprite.svg"))
-        .pipe(gulp.dest("build/img"));
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("html", function() {
-    return gulp.src("source/*.html")
-        .pipe(posthtml([
-        include()
-        ]))
-        .pipe(gulp.dest("build"));
-})
-
-gulp.task("build", gulp.series("css", "sprite", "html"));
-gulp.task("start", gulp.series("build", "server"));
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
 
 gulp.task("copy", function() {
-    return gulp.src([
-        "source/fonts/**/*.{woff,woff2}",
-        "source/img/**",
-        "source/js/**",
-        "source/*.ico"
-    ], {
-        base: "source"
-    })
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
     .pipe(gulp.dest("build"));
 });
 
 gulp.task("clean", function() {
-    return del("build");
+  return del("build");
 });
 
 gulp.task("build", gulp.series(
-    "clean",
-    "copy",
-    "css",
-    "sprite",
-    "html"
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
 ));
+
+gulp.task("start", gulp.series("build", "server"));
